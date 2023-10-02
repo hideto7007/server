2a59c214970b30b477b2cd75cd9c5c0f5c496020
hideto tsuzuki <ma_kux@icloud.com>
2023-10-02 22:32:05 +0900

    単体テスト実装

--- controllers/price_management_controllers.go
+++ controllers/price_management_controllers.go
@@ -2,11 +2,11 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 	"server/common"
 
 	"github.com/gin-gonic/gin"
-	// "github.com/gin-gonic/gin"
 )
 
 type PriceInfo struct {
@@ -19,7 +19,7 @@ type Response struct {
 	Error     string    `json:"error,omitempty"`
 }
 
-func priceCalc(moneyReceived, bouns, fixedCost, loan, private int) PriceInfo {
+func PriceCalc(moneyReceived, bouns, fixedCost, loan, private int) PriceInfo {
 
 	var priceinfo PriceInfo
 	priceinfo.LeftAmount = moneyReceived - fixedCost - loan - private
@@ -37,38 +37,15 @@ func GetPriceInfo(c *gin.Context) {
 	data, err := common.IntgetPrameter(c, "money_received", "bouns", "fixed_cost", "loan", "private")
 
 	if err == nil {
-		res := priceCalc(data["money_received"], data["bouns"], data["fixed_cost"], data["loan"], data["private"])
+		res := PriceCalc(data["money_received"], data["bouns"], data["fixed_cost"], data["loan"], data["private"])
 
 		response := Response{PriceInfo: res}
 
+		fmt.Println("debug", response)
+
 		c.JSON(http.StatusOK, gin.H{"message": response})
 	} else {
-		c.JSON(http.StatusOK, gin.H{"message": err})
+		c.JSON(http.StatusBadRequest, gin.H{"message": err})
 	}
 
 }
-
-// func GetPriceInfo(Response_Writer http.ResponseWriter, req *http.Request) {
-
-// 	// レスポンスのContent-Typeを設定
-// 	Response_Writer.Header().Set("Content-Type", "application/json; charset=utf-8")
-
-// 	// HTTPリクエストからクエリパラメータを取得してパラメーラ値を整数値に変換
-// 	data, err := common.IntgetPrameter(req, "money_received", "bouns", "fixed_cost", "loan", "private")
-
-// 	var response Response
-// 	if err == nil {
-// 		res := priceCalc(data["money_received"], data["bouns"], data["fixed_cost"], data["loan"], data["private"])
-// 		response.PriceInfo = res
-// 	} else {
-// 		// エラーメッセージをErrorフィールドに設定
-// 		response.Error = err.Error()
-// 	}
-
-// 	// Response型のデータをJSONに変換
-// 	jsonResponse, _ := json.Marshal(response)
-
-// 	// JSONレスポンスを書き込む
-// 	Response_Writer.WriteHeader(http.StatusOK)
-// 	Response_Writer.Write(jsonResponse)
-// }
