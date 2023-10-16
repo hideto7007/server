35f8d1550280ddbc2c4425da1109ce19053d4e8d
hideto tsuzuki <ma_kux@icloud.com>
2023-10-16 23:43:37 +0900

    テストデータ削除する関数実装中

--- common/common.go
+++ common/common.go
@@ -3,7 +3,10 @@ package common
 
 import (
 	// "fmt"
+	"database/sql"
 	"fmt"
+	"log"
+	"server/config"
 	"strconv"
 	"time"
 
@@ -102,6 +105,38 @@ func (cf *commonFetcherImpl) StrToInt(str string) (int, error) {
 		return replaceInt, err
 	}
 	return replaceInt, nil
+}
+
+// この関数はテストデータを削除するための独立関数
+func TestDataDelete() error {
+	db, err := sql.Open("postgres", config.DataSourceName)
+	if err != nil {
+		log.Printf("sql.Open error %s", err)
+	}
+	db.Close()
+
+	// トランザクションを開始
+	tx, err := db.Begin()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	deleteStatement := `
+		DELETE FROM public.incomeforecast_incomeforecastdata
+		WHERE update_user = 'user123';
+	`
+
+	if _, err = tx.Exec(deleteStatement); err != nil {
+		tx.Rollback()
+		fmt.Println(err)
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	return nil
 }
 
 // クエリーパラメータを整数値でまとめたマップで返す。
--- test/anuual_income_test.go
+++ test/anuual_income_test.go
@@ -1,6 +1,8 @@
 package common
 
 import (
+	"errors"
+	"server/common"
 	"server/config"
 	"server/models"
 	"testing"
@@ -10,21 +12,25 @@ import (
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/google/uuid"
+	_ "github.com/lib/pq"
 	"github.com/stretchr/testify/assert"
 )
 
-type MockIncomeData struct {
-	IncomeForecastID uuid.UUID
-	PaymentDate      string
-	Age              string
-	Industry         string
-	TotalAmount      int
-	DeductionAmount  int
-	TakeHomeAmount   int
-	Classification   string
-	UserID           int
-}
+type (
+	MockIncomeData struct {
+		IncomeForecastID uuid.UUID
+		PaymentDate      string
+		Age              string
+		Industry         string
+		TotalAmount      int
+		DeductionAmount  int
+		TakeHomeAmount   int
+		Classification   string
+		UserID           int
+	}
+)
 
+// func NewMocks
 func TestGetIncomeDataInRange(t *testing.T) {
 	t.Run("success TestGetIncomeDataInRange", func(t *testing.T) {
 		// テスト用のDBモックを作成
@@ -294,5 +300,241 @@ func TestGetYearsIncomeAndDeduction(t *testing.T) {
 		assert.Error(t, err)
 
 		t.Log(err)
+	})
+}
+
+func TestInsertIncome(t *testing.T) {
+	t.Run("success TestInsertIncome", func(t *testing.T) {
+		// テストデータを作成
+
+		testData := []models.InsertIncomeData{
+			{
+				PaymentDate:     "9999-01-01",
+				Age:             "30",
+				Industry:        "IT",
+				TotalAmount:     "1000",
+				DeductionAmount: "200",
+				TakeHomeAmount:  "800",
+				UpdateUser:      "user123",
+				Classification:  "A",
+				UserID:          "1",
+			},
+		}
+		// テスト用のPostgreSQLDataFetcherを作成
+		db, mock, err := sqlmock.New()
+		if err != nil {
+			t.Fatalf("Error creating mock database: %v", err)
+		}
+		defer db.Close()
+
+		// PostgreSQLDataFetcherに作成したモックDBを設定
+		dbFetcher := models.NewPostgreSQLDataFetcher(config.DataSourceName)
+
+		// モックの準備
+		mock.ExpectBegin()
+		mock.ExpectExec("INSERT INTO public.incomeforecast_incomeforecastdata (.+)").
+			WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg()).
+			WillReturnResult(sqlmock.NewResult(1, 1))
+		mock.ExpectCommit()
+
+		// InsertIncomeメソッドを呼び出し
+		err = dbFetcher.InsertIncome(testData)
+
+		// エラーがないことを検証
+		assert.NoError(t, err)
+
+		t.Log(err)
+	})
+	t.Run("error TestInsertIncome", func(t *testing.T) {
+		// テストデータを作成
+		testData := []models.InsertIncomeData{
+			{
+				PaymentDate:     "9999-01-01",
+				Age:             "30",
+				Industry:        "IT",
+				TotalAmount:     "3333",
+				DeductionAmount: "2222",
+				TakeHomeAmount:  "1111",
+				UpdateUser:      "user123",
+				Classification:  "A",
+				UserID:          "999", // pkの値を違反させてエラー確認する
+			},
+		}
+		// テスト用のPostgreSQLDataFetcherを作成
+		db, mock, err := sqlmock.New()
+		if err != nil {
+			t.Fatalf("Error creating mock database: %v", err)
+		}
+		defer db.Close()
+
+		// PostgreSQLDataFetcherに作成したモックDBを設定
+		dbFetcher := models.NewPostgreSQLDataFetcher(config.DataSourceName)
+
+		// モックの準備
+		mock.ExpectBegin()
+		mock.ExpectExec("INSERT INTO public.incomeforecast_incomeforecastdata (.+)").
+			WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg()).
+			WillReturnResult(sqlmock.NewErrorResult(errors.New("ERROR"))).
+			WillReturnError(errors.New("INSERT FAILED"))
+		mock.ExpectCommit()
+
+		// InsertIncomeメソッドを呼び出し
+		err = dbFetcher.InsertIncome(testData)
+
+		// エラーが発生すること
+		assert.Error(t, err)
+
+		t.Log("error", err)
+	})
+}
+
+func TestUpdateIncome(t *testing.T) {
+	t.Run("success TestUpdateIncome", func(t *testing.T) {
+		// テストデータを作成
+		testData := []models.UpdateIncomeData{
+			{
+				IncomeForecastID: "ecdb3762-9417-419d-c458-42d90a63bfd0", // 既存のレコードの ID
+				PaymentDate:      "9999-01-01",
+				Age:              "30",
+				Industry:         "IT",
+				TotalAmount:      "1200",
+				DeductionAmount:  "250",
+				TakeHomeAmount:   "950",
+				Classification:   "B",
+			},
+		}
+		// テスト用のPostgreSQLDataFetcherを作成
+		db, mock, err := sqlmock.New()
+		if err != nil {
+			t.Fatalf("Error creating mock database: %v", err)
+		}
+		defer db.Close()
+
+		// PostgreSQLDataFetcherに作成したモックDBを設定
+		dbFetcher := models.NewPostgreSQLDataFetcher(config.DataSourceName)
+
+		// モックの準備
+		mock.ExpectBegin()
+		mock.ExpectExec("UPDATE public.incomeforecast_incomeforecastdata SET (.+) WHERE incomeforecastid = (.+)").
+			WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg()).
+			WillReturnResult(sqlmock.NewResult(1, 1))
+		mock.ExpectCommit()
+
+		// UpdateIncome メソッドを呼び出し
+		err = dbFetcher.UpdateIncome(testData)
+
+		// エラーがないことを検証
+		assert.NoError(t, err)
+	})
+
+	t.Run("error TestUpdateIncome", func(t *testing.T) {
+		// テストデータを作成
+		testData := []models.UpdateIncomeData{
+			{
+				IncomeForecastID: "ecdb3762-9417-419d-c458-42d90a63bfrr656gtgtgtfd0", // エラー用のuuid
+				PaymentDate:      "9999-01-01",
+				Age:              "30",
+				Industry:         "IT",
+				TotalAmount:      "1200",
+				DeductionAmount:  "250",
+				TakeHomeAmount:   "950",
+				Classification:   "B",
+			},
+		}
+		// テスト用のPostgreSQLDataFetcherを作成
+		db, mock, err := sqlmock.New()
+		if err != nil {
+			t.Fatalf("Error creating mock database: %v", err)
+		}
+		defer db.Close()
+
+		// PostgreSQLDataFetcherに作成したモックDBを設定
+		dbFetcher := models.NewPostgreSQLDataFetcher(config.DataSourceName)
+
+		// モックの準備
+		mock.ExpectBegin()
+		mock.ExpectExec("UPDATE public.incomeforecast_incomeforecastdata SET (.+) WHERE incomeforecastid = (.+)").
+			WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg()).
+			WillReturnResult(sqlmock.NewErrorResult(errors.New("ERROR"))).
+			WillReturnError(errors.New("UPDATE FAILED"))
+		mock.ExpectCommit()
+
+		// UpdateIncome メソッドを呼び出し
+		err = dbFetcher.UpdateIncome(testData)
+
+		// エラーが発生すること
+		assert.Error(t, err)
+
+		t.Log("error", err)
+	})
+}
+
+func TestDeleteIncome(t *testing.T) {
+	t.Run("success TestDeleteIncome", func(t *testing.T) {
+		// テストデータを作成
+		testData := []models.DeleteIncomeData{
+			{
+				IncomeForecastID: "57cbdd21-3cce-42f2-ad3c-2f727d7edae7", // 既存のレコードの ID
+			},
+		}
+		// テスト用のPostgreSQLDataFetcherを作成
+		db, mock, err := sqlmock.New()
+		if err != nil {
+			t.Fatalf("Error creating mock database: %v", err)
+		}
+		defer db.Close()
+
+		// PostgreSQLDataFetcherに作成したモックDBを設定
+		dbFetcher := models.NewPostgreSQLDataFetcher(config.DataSourceName)
+
+		// モックの準備
+		mock.ExpectBegin()
+		mock.ExpectExec("DELETE FROM public.incomeforecast_incomeforecastdata WHERE incomeforecastid = (.+)").
+			WithArgs(sqlmock.AnyArg()).
+			WillReturnResult(sqlmock.NewResult(1, 1))
+		mock.ExpectCommit()
+
+		// DeleteIncome メソッドを呼び出し
+		err = dbFetcher.DeleteIncome(testData)
+
+		// エラーがないことを検証
+		assert.NoError(t, err)
+	})
+	t.Run("error TestDeleteIncome", func(t *testing.T) {
+		// テストデータを作成
+		testData := []models.DeleteIncomeData{
+			{
+				IncomeForecastID: "ecdb3762-9417-419d-c458-42d9frfrde450a63bfd0", // エラー用のuuid
+			},
+		}
+		// テスト用のPostgreSQLDataFetcherを作成
+		db, mock, err := sqlmock.New()
+		if err != nil {
+			t.Fatalf("Error creating mock database: %v", err)
+		}
+		defer db.Close()
+
+		// PostgreSQLDataFetcherに作成したモックDBを設定
+		dbFetcher := models.NewPostgreSQLDataFetcher(config.DataSourceName)
+
+		// モックの準備
+		mock.ExpectBegin()
+		mock.ExpectExec("DELETE FROM public.incomeforecast_incomeforecastdata WHERE incomeforecastid = (.+)").
+			WithArgs(sqlmock.AnyArg()).
+			WillReturnResult(sqlmock.NewErrorResult(errors.New("ERROR"))).
+			WillReturnError(errors.New("DELETE FAILED"))
+		mock.ExpectCommit()
+
+		// DeleteIncome メソッドを呼び出し
+		err = dbFetcher.DeleteIncome(testData)
+
+		// エラーが発生すること
+		assert.Error(t, err)
+
+		t.Log("error", err)
+
+		err = common.TestDataDelete()
+
+		t.Log("検知", err)
 	})
 }
