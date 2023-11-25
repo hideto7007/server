e452d14671845cc20b1ddbe518fec9e77e3e12d3
hideto tsuzuki <ma_kux@icloud.com>
2023-11-25 20:15:39 +0900

    単体テスト修正

--- test/price_management_controllers_test.go
+++ test/price_management_controllers_test.go
@@ -18,10 +18,10 @@ func TestPriceCalc(t *testing.T) {
 		// テストケース1: 正常な整数の計算
 
 		var price controllers.PriceManagementFetcher = controllers.NewPriceManagementFetcher()
-		result := price.PriceCalc(300, 100, 50, 50, 50)
+		result := price.PriceCalc(300, 100, 50, 50, 50, 30)
 
 		assert.Equal(t, 150, result.LeftAmount)
-		assert.Equal(t, 1900, result.TotalAmount)
+		assert.Equal(t, 1870, result.TotalAmount)
 
 		t.Logf("result.LeftAmount: %d", result.LeftAmount)
 		t.Logf("result.TotalAmount: %d", result.TotalAmount)
@@ -32,14 +32,14 @@ func TestGetPriceInfo(t *testing.T) {
 	t.Run("success GetPriceInfoApi()", func(t *testing.T) {
 		// テスト用のGinコンテキストを作成
 		c, _ := gin.CreateTestContext(httptest.NewRecorder())
-		c.Request = httptest.NewRequest("GET", "/?money_received=300&bouns=100&fixed_cost=50&loan=50&private=50", nil)
+		c.Request = httptest.NewRequest("GET", "/?money_received=300&bouns=100&fixed_cost=50&loan=50&private=50&insurance=30", nil)
 
 		var common common.CommonFetcher = common.NewCommonFetcher()
-		paramMap, err := common.IntgetPrameter(c, "money_received", "bouns", "fixed_cost", "loan", "private")
+		paramMap, err := common.IntgetPrameter(c, "money_received", "bouns", "fixed_cost", "loan", "private", "insurance")
 
 		// PriceCalc 関数をモック化
 		var price controllers.PriceManagementFetcher = controllers.NewPriceManagementFetcher()
-		res := price.PriceCalc(paramMap["money_received"], paramMap["bouns"], paramMap["fixed_cost"], paramMap["loan"], paramMap["private"])
+		res := price.PriceCalc(paramMap["money_received"], paramMap["bouns"], paramMap["fixed_cost"], paramMap["loan"], paramMap["private"], paramMap["insurance"])
 
 		// GetPriceInfoApi 関数を呼び出し
 		price.GetPriceInfoApi(c)
@@ -57,7 +57,7 @@ func TestGetPriceInfo(t *testing.T) {
 
 		assert.Nil(t, err)
 		assert.Equal(t, 150, response.PriceInfo.LeftAmount)
-		assert.Equal(t, 1900, response.PriceInfo.TotalAmount)
+		assert.Equal(t, 1870, response.PriceInfo.TotalAmount)
 
 		t.Logf("response.PriceInfo.LeftAmount: %d", response.PriceInfo.LeftAmount)
 		t.Logf("response.PriceInfo.TotalAmount: %d", response.PriceInfo.TotalAmount)
@@ -67,10 +67,10 @@ func TestGetPriceInfo(t *testing.T) {
 	t.Run("error case GetPriceInfoApi()", func(t *testing.T) {
 		// エラーを引き起こすリクエストをシミュレート
 		c, _ := gin.CreateTestContext(httptest.NewRecorder())
-		c.Request = httptest.NewRequest("GET", "/?money_received=300&bouns=100&fixed_cost=notanumber&loan=50&private=50", nil)
+		c.Request = httptest.NewRequest("GET", "/?money_received=300&bouns=100&fixed_cost=notanumber&loan=50&private=50&insurance=30", nil)
 
 		var common common.CommonFetcher = common.NewCommonFetcher()
-		paramMap, err := common.IntgetPrameter(c, "money_received", "bouns", "fixed_cost", "loan", "private")
+		paramMap, err := common.IntgetPrameter(c, "money_received", "bouns", "fixed_cost", "loan", "private", "insurance")
 
 		// GetPriceInfoApi 関数を呼び出し
 		var price controllers.PriceManagementFetcher = controllers.NewPriceManagementFetcher()
