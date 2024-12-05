2e36364ebeda0e23ee734fa8b95e18f43a80bb4e
hideto <ma_kux@icloud.com>
2024-12-05 16:27:12 +0900

    PutCheck単体テスト実装

--- models/sign_test.go
+++ models/sign_test.go
@@ -8,7 +8,10 @@ import (
 	"server/utils"
 	"testing"
 
+	mock_utils "server/mock/utils"
+
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/golang/mock/gomock"
 	_ "github.com/lib/pq"
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/crypto/bcrypt"
@@ -529,6 +532,239 @@ func TestPutSignInEdit(t *testing.T) {
 		assert.Contains(t, err.Error(), "transaction begin error")
 
 		t.Log("transaction begin error PutSignInEdit log", err)
+	})
+}
+
+func TestPutCheck(t *testing.T) {
+	t.Run("PutCheck クエリー実行時エラー", func(t *testing.T) {
+		// テスト用のDBモックを作成
+		dbFetcher, mock, err := NewSignDataFetcher(
+			"test",
+			utils.NewUtilsFetcher(utils.JwtSecret),
+		)
+		if err != nil {
+			t.Fatalf("Error creating DB mock: %v", err)
+		}
+
+		// テスト対象のデータ
+		requestData := RequestSignInEditData{
+			UserId:       "1",
+			UserName:     "test@exmple.com",
+			UserPassword: "Test12345!",
+		}
+
+		// クエリ実行時にエラーを返すようにモックを設定
+		mock.ExpectQuery(regexp.QuoteMeta(DB.GetSignInSyntax)).
+			WithArgs(requestData.UserName).
+			WillReturnError(fmt.Errorf("クエリの実行に失敗しました"))
+
+		// テストを実行
+		_, err = dbFetcher.PutCheck(requestData)
+
+		// クエリエラーが発生したことを確認
+		assert.Error(t, err)
+		assert.EqualError(t, err, "クエリの実行に失敗しました")
+	})
+
+	t.Run("PutCheck rows.Scan時にエラー発生 UserIdで検証", func(t *testing.T) {
+		// テスト用のDBモックを作成
+		dbFetcher, mock, err := NewSignDataFetcher(
+			"test",
+			utils.NewUtilsFetcher(utils.JwtSecret),
+		)
+		if err != nil {
+			t.Fatalf("Error creating DB mock: %v", err)
+		}
+
+		// テスト対象のデータ
+		requestData := RequestSignInEditData{
+			UserId:       "test",
+			UserName:     "test@exmple.com",
+			UserPassword: "Test12345!",
+		}
+
+		// テスト用の行データを設定
+		rows := sqlmock.NewRows([]string{
+			"user_id", "user_name", "user_password",
+		}).AddRow(
+			"test",
+			"test@exmple.com",
+			"Test12345!",
+		)
+
+		// クエリ実行時にエラーを返すようにモックを設定
+		mock.ExpectQuery(regexp.QuoteMeta(DB.GetSignInSyntax)).
+			WithArgs(requestData.UserName).
+			WillReturnRows(rows)
+
+		// テストを実行
+		_, err = dbFetcher.PutCheck(requestData)
+
+		// クエリエラーが発生したことを確認
+		assert.Error(t, err)
+	})
+
+	t.Run("PutCheck rows.Err()にエラー発生", func(t *testing.T) {
+		// テスト用のDBモックを作成
+		dbFetcher, mock, err := NewSignDataFetcher(
+			"test",
+			utils.NewUtilsFetcher(utils.JwtSecret),
+		)
+		if err != nil {
+			t.Fatalf("Error creating DB mock: %v", err)
+		}
+
+		// テスト対象のデータ
+		requestData := RequestSignInEditData{
+			UserId:       "1",
+			UserName:     "test@exmple.com",
+			UserPassword: "Test12345!",
+		}
+
+		// 正常な行データを用意
+		rows := sqlmock.NewRows([]string{
+			"user_id", "user_name", "user_password",
+		}).AddRow(
+			1,
+			"test@exmple.com",
+			"Test12345!",
+		)
+
+		// クエリ結果が返った後にエラーを発生させる
+		rows.RowError(0, fmt.Errorf("forced row error"))
+
+		// クエリ実行時にエラーを返すようにモックを設定
+		mock.ExpectQuery(regexp.QuoteMeta(DB.GetSignInSyntax)).
+			WithArgs(requestData.UserName).
+			WillReturnRows(rows)
+
+		// テストを実行
+		_, err = dbFetcher.PutCheck(requestData)
+
+		// クエリエラーが発生したことを確認
+		assert.Error(t, err)
+		assert.Contains(t, err.Error(), "forced row error")
+	})
+
+	t.Run("PutCheck 成功 パスワード更新", func(t *testing.T) {
+		// gomock のコントローラを作成
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		// UtilsFetcher のモックを作成
+		mockUtilsFetcher := mock_utils.NewMockUtilsFetcher(ctrl)
+
+		mockUtilsFetcher.EXPECT().
+			CompareHashPassword(gomock.Any(), gomock.Any()).
+			Return(fmt.Errorf("不一致"))
+
+		// テスト用のDBモックを作成
+		dbFetcher, mock, err := NewSignDataFetcher(
+			"test",
+			mockUtilsFetcher,
+		)
+		if err != nil {
+			t.Fatalf("Error creating DB mock: %v", err)
+		}
+
+		// テスト対象のデータ
+		requestData := RequestSignInEditData{
+			UserId:       "1",
+			UserName:     "test@exmple.com",
+			UserPassword: "Test12345!",
+		}
+
+		// 平文のパスワードをハッシュ化
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(requestData.UserPassword), bcrypt.DefaultCost)
+		if err != nil {
+			t.Fatalf("Error hashing password: %v", err)
+		}
+
+		mockData := []SignInData{
+			{
+				UserId:       1,
+				UserName:     "test@exmple.com",
+				UserPassword: string(hashedPassword),
+			},
+		}
+
+		expectedData := "パスワード更新"
+
+		// テスト用の行データを設定
+		rows := sqlmock.NewRows([]string{
+			"user_id", "user_name", "user_password",
+		})
+
+		for _, data := range mockData {
+			rows.AddRow(
+				data.UserId,
+				data.UserName,
+				data.UserPassword,
+			)
+		}
+
+		mock.ExpectQuery(regexp.QuoteMeta(DB.GetSignInSyntax)).
+			WithArgs(requestData.UserName).
+			WillReturnRows(rows)
+
+		// テストを実行
+		result, err := dbFetcher.PutCheck(requestData)
+
+		// エラーがないことを検証
+		assert.NoError(t, err)
+
+		t.Log("result : ", result)
+		// 取得したデータが期待値と一致することを検証
+		assert.Equal(t, expectedData, result)
+
+		// モックが期待通りのクエリを受け取ったか確認
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("mock check: %s", err)
+		}
+	})
+
+	t.Run("PutCheck 成功 ユーザー名更新", func(t *testing.T) {
+		// テスト用のDBモックを作成
+		dbFetcher, mock, err := NewSignDataFetcher(
+			"test",
+			utils.NewUtilsFetcher(utils.JwtSecret),
+		)
+		if err != nil {
+			t.Fatalf("Error creating DB mock: %v", err)
+		}
+
+		// テスト対象のデータ
+		requestData := RequestSignInEditData{
+			UserId:       "1",
+			UserName:     "test@exmple.com",
+			UserPassword: "Test12345!",
+		}
+
+		expectedData := "ユーザー名更新"
+
+		// テスト用の行データを設定
+		rows := sqlmock.NewRows([]string{
+			"user_id", "user_name", "user_password",
+		})
+
+		mock.ExpectQuery(regexp.QuoteMeta(DB.GetSignInSyntax)).
+			WithArgs(requestData.UserName).
+			WillReturnRows(rows)
+
+		// テストを実行
+		result, err := dbFetcher.PutCheck(requestData)
+
+		// エラーがないことを検証
+		assert.NoError(t, err)
+
+		t.Log("result : ", result)
+		// 取得したデータが期待値と一致することを検証
+		assert.Equal(t, expectedData, result)
+
+		// モックが期待通りのクエリを受け取ったか確認
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("mock check: %s", err)
+		}
 	})
 }
 
